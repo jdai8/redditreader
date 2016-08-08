5c9c8d6f81bbdaa84dec4924f2f1298790c3a06f
Jack Dai <jdai88@gmail.com>
2016-08-07 22:59:23 -0400

    Configure heroku deploy

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const request = require('request');
 
 const app = express();
 
-app.use('/', express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.use('/api', (req, res) => {
-  console.log(req.path);
-
-  const options = {
-    uri: `https://reddit.com${req.path}.json`,
-    headers: {
-      'User-Agent': 'material reddit app',
-    },
-    qs: {
-      'limit': 20,
-      'raw_json': 1,
-    },
-  };
-
-  request(options, (err, r, body) => {
-
-    if (!err && r.statusCode === 200) {
-      res.send(body);
-    }
-  });
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-const port = 80;
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Express started on port ${port}.`));
